fad8ecef70e11bcc649e4a6fa3582937a576585e
Henrik Reinstädtler <reinstaedtler@stud.uni-heidelberg.de>
2020-10-02 13:40:37 +0200

    Correct position of build tag in parseConfig.go

--- pkg/pdfcpu/parseConfig.go
+++ pkg/pdfcpu/parseConfig.go
@@ -1,3 +1,5 @@
+// +build !js
+
 /*
 Copyright 2020 The pdfcpu Authors.
 
@@ -13,7 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-// +build !js
+
 
 package pdfcpu
 
