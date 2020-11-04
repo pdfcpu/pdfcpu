718a3f0d81d670ee829a19ec652feb8e1f98a288
Horst Rutter <hhrutter@gmail.com>
2020-11-04 01:44:35 +0100

    Bump version

--- pkg/pdfcpu/version.go
+++ pkg/pdfcpu/version.go
@@ -23,7 +23,7 @@ import (
 )
 
 // VersionStr is the current pdfcpu version.
-var VersionStr = "v0.3.6 dev"
+var VersionStr = "v0.3.7 dev"
 
 // Version is a type for the internal representation of PDF versions.
 type Version int
