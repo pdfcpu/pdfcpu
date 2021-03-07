220d0c9e8e70302dbf61da598b98f3e6801c3516
Horst Rutter <hhrutter@gmail.com>
2021-03-07 02:08:23 +0100

    Fix #305

--- pkg/pdfcpu/string.go
+++ pkg/pdfcpu/string.go
@@ -168,7 +168,8 @@ func Unescape(s string) ([]byte, error) {
 			continue
 		}
 
-		if !strings.ContainsRune("nrtbf()01234567", rune(c)) {
+		// Relax for issue 305 and also accept "\ ".
+		if !strings.ContainsRune(" nrtbf()01234567", rune(c)) {
 			return nil, errors.Errorf("Unescape: illegal escape sequence \\%c detected", c)
 		}
 
